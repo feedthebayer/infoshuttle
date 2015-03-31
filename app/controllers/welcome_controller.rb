a61b9a9a7618102f30269fc92d3b1f149910e05b
Brandon Bayer <feedthebayer@users.noreply.github.com>
2015-03-30 19:49:06 -0500

    style: fix Hound issues

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def show
-    if !current_user || params[:id] == 'landing'
-      render 'landing'
+    if !current_user || params[:id] == "landing"
+      render "landing"
     else
       redirect_to current_user
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
-  get '/landing' => 'welcome#show', id: 'landing'
+  get "/landing" => "welcome#show", id: "landing"
 
   post "oauth/callback" => "oauths#callback"
   get "oauth/callback" => "oauths#callback"
-  get "sign_in/:provider" => "oauths#oauth", as: 'auth_at_provider'
-  delete 'sign_out' => 'user_sessions#destroy'
+  get "sign_in/:provider" => "oauths#oauth", as: "auth_at_provider"
+  delete "sign_out" => "user_sessions#destroy"
 
   resources :users, only: [:show, :edit]
 
   resources :wikis, only: [:create, :index]
-  resources :wikis, path: '', only: [:show, :update, :destroy] do
+  resources :wikis, path: "", only: [:show, :update, :destroy] do
     resources :pages, only: [:create, :show, :update, :destroy]
   end
 
-  root 'welcome#show'
+  root "welcome#show"
 end
