631c547da198db1755b335e1879ab06b578484b6
Brandon Bayer <feedthebayer@users.noreply.github.com>
2015-04-06 19:40:10 -0500

    fix: all wikis showing on user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   load_and_authorize_resource
 
   def show
-    @wikis = Wiki.accessible_by(current_ability)
+    @wikis = @user.wikis.accessible_by(current_ability)
     @new_wiki = Wiki.new
   end
 end
