f3209a6e3c1e125f58429214952be865d5e2768d
Brandon Bayer <feedthebayer@users.noreply.github.com>
2015-04-25 15:24:45 -0500

    fix(collaboration): prevent duplicate collaborations

--- app/models/collaboration.rb
+++ app/models/collaboration.rb
@@ -3,6 +3,7 @@ class Collaboration < ActiveRecord::Base
   belongs_to :wiki
 
   validates :wiki, :user, presence: true
+  validates_uniqueness_of :user_id, scope: :wiki_id
 
   default_scope { order("created_at ASC") }
 end
